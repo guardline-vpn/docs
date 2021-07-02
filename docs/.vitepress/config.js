9656022d7e4a3519eff433b77215c8d2e423c762
JamieCurnow <jamie@island-web.ca>
2021-07-03 00:27:20 +0100

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Guardline Docs',
+  description: 'How to use Guardline VPN',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: []
+  }
+}
